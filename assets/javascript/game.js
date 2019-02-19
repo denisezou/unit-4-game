31c1f1371d0f967659f5957302d8aba8d0f9f2dd
DenZou <dzou@u.northwestern.edu>
2019-02-19 12:54:04 -0600

    push2

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//1 define variables
+//a) hold random number picked by computer (held steady until a Win or Lose)
+//b) hold user sum 
+//c) hold crystal button's random number (held steady until a win or lose)
+
+//2 Generate random computer number with Math.floor.math.random()
+
+//3 Generate 4 crystal buttons' value number with Math.floor.math.random()
+
+//4 If usersum = random computer number, win ++
+//a) Display "You win!!"
+
+//5 If usersum > random computer number, lose ++ 
+//a) Display "You lose!!"
+
+//6 On click events for crystal buttons
+//a) when click on crystal, add its value to the user sum
+
+//7) reset if statement (if Win or Lose)
+//a) when you reset, set user sum to 0, set crystal button value to new random number, set computer random number to new random number.
+
+//8) For loop for the function initializeFunction to add on click values only until Win or Lose, at which point you reset.
